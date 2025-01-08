f8062896437708946fc77cfc86466ce7ced4f3cc
John Kelly <jkelly3rd@gmail.com>
2025-01-08 12:06:07 -0500

    Move with evac

--- cali_fires_start.R
+++ cali_fires_start.R
@@ -10,12 +10,6 @@ library(geojsonio)
 
 # source feeds from calfire
 
-# evacs: https://www.arcgis.com/home/item.html?id=9be87abf402b40f6bb703f6d7a2b39c5 and
-# https://www.arcgis.com/home/item.html?id=9be87abf402b40f6bb703f6d7a2b39c5&view=service#overview
-# california perimeters: 
-# option: https://gis.data.ca.gov/datasets/CALFIRE-Forestry::california-fire-perimeters-all/explore
-
-
 
 # Define the URL and the destination file path
 url <- "https://services1.arcgis.com/jUJYIo9tSA7EHvfZ/arcgis/rest/services/CA_Perimeters_NIFC_FIRIS_public_view/FeatureServer/0/query?where=1%3D1&objectIds=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&returnEnvelope=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&collation=&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnTrueCurves=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token="
@@ -56,11 +50,6 @@ cali_fires <- cali_fires %>%
 cali_fires <- cali_fires %>%
   mutate(acres_burned = round(acres_burned))
 
-# calculate acres burned based on area of the revised polygon
-cali_fires <- cali_fires %>%
-  mutate(acres_burned = ifelse(acres_burned == 0, st_area(geometry) * 0.000247105, acres_burned))
-
-
 # add a field called timestamp that is the current system time but in the Pacific time zone
 cali_fires <- cali_fires %>%
   mutate(timestamp = as.POSIXct(Sys.time(), tz = "America/Los_Angeles"))
@@ -82,6 +71,39 @@ file.rename("data/latest_cali_fires.geojson", paste0("data/archive_cali_fires_",
 
 # export geojson file to data directory
 geojson_write(cali_fires, file = "data/latest_cali_fires.geojson")
+
+
+# evacs: https://www.arcgis.com/home/item.html?id=9be87abf402b40f6bb703f6d7a2b39c5 and
+# https://www.arcgis.com/home/item.html?id=9be87abf402b40f6bb703f6d7a2b39c5&view=service#overview
+
+evac_url <- "https://services3.arcgis.com/uknczv4rpevve42E/arcgis/rest/services/CA_EVACUATIONS_PROD/FeatureServer/0/query?where=0%3D0&objectIds=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&returnEnvelope=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&collation=&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnTrueCurves=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token="
+evac_destfile <- "data/CA_evacuations.geojson"
+# Download the file using httr
+response <- GET(evac_url)
+writeBin(content(response, "raw"), evac_destfile)
+
+
+# Load the downloaded geojson file
+cali_evac <- st_read(evac_destfile) %>% janitor::clean_names()
+# Make geometries valid
+cali_evac <- st_make_valid(cali_evac)
+ 
+# create a quick leaflet map showing the perimters from cali_fires on a map with a satellite view provider layer
+#quick_evacmap <- leaflet(cali_evac) %>%
+#  addProviderTiles(providers$Esri.WorldImagery) %>%
+#  addPolygons(color = "red", weight = 2, opacity = 1, fillOpacity = 0.2) %>%
+#  addLegend("bottomright", colors = "red", labels = "Latest Evacs") 
+# add a popup that includes the fire name, acres burned, containment percentage and update date
+
+# remove latest_cali_fires.geojson in data directory
+file.remove("data/latest_cali_evac.geojson")
+
+# export geojson file to data directory
+geojson_write(cali_evac, file = "data/latest_cali_evac.geojson")
+
+
+
+
 
 
 
