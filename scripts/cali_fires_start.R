ff5b23b9dbbddd08040ed17d20a3b641290df5aa
John Kelly <jkelly3rd@gmail.com>
2025-01-22 10:43:14 -0500

    Aa

--- scripts/cali_fires_start.R
+++ scripts/cali_fires_start.R
@@ -29,7 +29,7 @@ cali_fires <- cali_fires %>%
 
 # Filter dataset to only those updated in the last 14 days
 cali_fires <- cali_fires %>%
-  filter(poly_date_current >= Sys.Date() - 10)
+  filter(poly_date_current >= Sys.Date() - 15)
 
 # Removing the code letters etc from mission names to create a standard common fire name
 cali_fires <- cali_fires %>%
